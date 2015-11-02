6eb2f50c47e1cb7781a17985ddb628a5c82a5122
saezluis <lsaez@pm.cl>
2015-11-02 19:00:59 -0300

    Agregado registro de los usuarios
    hace falta la pagina que captura la data

---
+++ registroUsuario.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <title>Inicio / Súbete</title>
+  </head>
+ <body>
+	<form method="post" action="">
+		<li>Nombre: <input type="text" name="nombre"></li>
+		<br>
+		<li>Apellido Paterno: <input type="text" name="apellido_paterno"></li>
+		<br>
+		<li>Apellido Materno: <input type="text" name="apellido_materno"></li>
+		<br>
+		<li>Sexo: <select name="sexo">
+					<option value="masculino">Masculino</option>
+					<option value="femenino">Femenino</option>
+				  </select></li>
+		<br>
+		<li>Fecha nacimiento: <input type="text" name="fecha_nacimiento"></li>
+		<br>
+		<li>Rut: <input type="text" name="rut"></li>
+		<br>
+		<li>Telefono/Movil: <input type="text" name="telefono"></li>
+		<br>
+		<li>Comuna residencia: <input type="text" name="comuna"></li>
+		<br>
+		<li>Correo electronico: <input type="text" name="email"></li>
+		<br>
+		<li>Clave: <input type="text" name="clave"></li>
+		<br>
+		<li><textarea name="tema_interes"> Temas de interes:</textarea></li>
+		<br>
+		<button type="submit">Enviar</submit>			
+	</form>
+ </body>
+ </html>
+ 
+ 
