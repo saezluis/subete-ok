2724caf26d72d5fc27032cf3086a2bc726e7813a
saezluis <lsaez@pm.cl>
2015-11-02 19:25:09 -0300

    Lista la parte de seguridad
    pagina cargada

--- videos.php
+++ videos.php
@@ -1,3 +1,26 @@
+<?php
+session_start();
+
+if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true)
+{
+
+}
+else
+{
+echo "<br/>" . "Esta pagina es solo para usuarios registrados." . "<br/>";
+echo "<br/>" . "<a href='login.php'>Hacer Login</a>";
+
+exit;
+}
+$now = time(); // checking the time now when home page starts
+
+if($now > $_SESSION['expire'])
+{
+session_destroy();
+echo "<br/><br />" . "Su sesion a terminado, <a href='login.php'> Necesita Hacer Login</a>";
+exit;
+}
+?>
 <!DOCTYPE html>
 <html lang="es">
   <head>
@@ -7,91 +30,81 @@
     <link rel="stylesheet" href="css/estilos.css">
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
     <script src="js/scripts-min.js"></script>
-	
-	<script type="text/javascript">
-	
-	// <![CDATA[
-	function showYouTubeThumbnails(data) {
-		/** USER SETTINGS ***********/
-		var highResThumbnails = false;			// set to false for normal (120px x 90px), set to true for high-resolution (320px x 240px)
-		var targetId = "videos";					// Id of element where the thumbnails will be inserted
-		/******************************/
 
-		var feed = data.feed;
-	 	var entries = feed.entry || [];
-	 	var targetObj = document.getElementById(targetId);
-
-	 	for (var i = 0; i < entries.length; i++) {
-	    		var entry = entries[i];
-	    		var link = entry['link'][0]['href'];
-	    		var thumbnailUrl;
-			var thumbnailWidth;
-			var thumbnailHeight;
-			var minimumHighResWidth = 320;	// minimum width for a high-resolution thumbnail
-
-			var thumbnailsObj = entry['media$group']['media$thumbnail'];
-			for (var j = 0; j < thumbnailsObj.length; j++) {
-				thumbnailUrl = thumbnailsObj[j]['url'];
-				thumbnailWidth = thumbnailsObj[j]['width'];
-				thumbnailHeight = thumbnailsObj[j]['height'];
-				if (thumbnailsObj[j]['width'] >= minimumHighResWidth && highResThumbnails || thumbnailsObj[j]['width'] < minimumHighResWidth && !highResThumbnails) {
-					break;
-				}
-			}
-
-	    		var newA = document.createElement('a');
-	    		newA.href = link;
-	    		newA.title = "YouTube video";
-
-	    		var newImg = document.createElement('img');
-	    		newImg.src = thumbnailUrl;
-	    		newImg.width = thumbnailWidth;
-	    		newImg.height = thumbnailHeight;
-	    		newImg.alt = "YouTube video thumbnail";
-			newA.appendChild(newImg);
-
-	    		targetObj.appendChild(newA);
-	  	}
-	}
-	// ]]>
-	
-	</script>
-	
-	
-	<script type="text/javascript">
-	
-	jQuery(document).ready(function($){
-
-		$('iframe[src*="youtube.com"]').each(function() {
-			var url = $(this).attr("src")
-			$(this).attr("src",url+"&amp;wmode=transparent")
-		}); 
-
-	});
-	
-	</script>
-	
-		
-	
-	<!-- This loads the YouTube IFrame Player API code -->
-<script src="http://www.youtube.com/player_api"></script>
 	
   </head>
   <body>
+	<?php
+	
+	include_once 'config.php';
+		
+		$conexion=mysqli_connect($host,$username,$password,$db_name) or die("Problemas con la conexión");
+		$acentos = $conexion->query("SET NAMES 'utf8'");
+
+		//Inserta comentarios del footer en la Base de Datos
+		if(isset($_POST['comentario'])){
+			$sugerencias = $_POST['comentario'];
+			
+			mysqli_query($conexion,"insert into sugerencia(rut,comentario) values ('$_REQUEST[rut_send]','$_REQUEST[comentario]')")
+			or die("Problemas con el insert de los servicios");
+			
+		}
+				
+		$login_email = $_SESSION['username'];
+				
+		$registros=mysqli_query($conexion,"select * from cuenta where email = '$login_email'") or die("Problemas en el select:".mysqli_error($conexion));
+			
+		if($reg=mysqli_fetch_array($registros)){
+		
+			$nombre = $reg['nombre'];
+			$rut = $reg['rut'];
+			$apellido_paterno = $reg['apellido_paterno'];
+			$apellido_materno = $reg['apellido_materno'];			
+			$foto_perfil = $reg['foto_perfil'];
+				
+		}
+		
+		$registrosSistema=mysqli_query($conexion,"select * from usuarios where correo = '$login_email'") or die("Problemas en el select:".mysqli_error($conexion));
+		
+		if($reg=mysqli_fetch_array($registrosSistema)){
+		
+			$sistema_web = $reg['sistema_web'];
+	
+		}
+		
+		if($sistema_web=='interno'){
+			$status_cata = 'display:zerocool;';			
+		}else{
+			$status_cata = 'display:none;';
+		}
+		
+		if($sistema_web=='interno'){
+			$status_bene = 'display:zerocool;';			
+		}else{
+			$status_bene = 'display:none;';
+		}
+	
+	?>
+	
     <header class="grupo">
-      <div class="caja base-100">
-        <div id="logo"><a href="index.html"><img src="img/logo.png"></a></div>
-        <div id="logo_royal"><a href="http://www.royalrental.cl" target="_blank" alt="Royal Rental"><img src="img/logo_royal.jpg"></a></div>
-        <div id="admin-header">
-          <div id="admin--data"><img src="img/user.jpg" class="circulo"><span class="nombre_usuario">Jorge Ortiz L.</span></div>
-          <div id="cuenta">
-            <ul>
-              <li><a href="mi-cuenta.html" class="micuenta">Mi cuenta</a></li>
-              <li><a href="#" class="cerrar">Cerrar</a></li>
-            </ul>
-          </div>
-        </div>
-      </div>
+	
+      <?php 	
+	    $nombre_user = $nombre." ".$apellido_paterno." ".$apellido_materno;
+        echo "<div class=\"caja base-100\">";
+        echo "<div id=\"logo\"><a href=\"index.php\"><img src=\"img/logo.png\"></a></div>";
+        echo "<div id=\"logo_royal\"><a href=\"http://www.royalrental.cl\" target=\"_blank\" alt=\"Royal Rental\"><img src=\"img/logo_royal.jpg\"></a></div>";
+        echo "<div id=\"admin-header\">";
+          echo "<div id=\"admin--data\"><img src=\"img/img-perfil/$foto_perfil\" class=\"circulo\"><span class=\"nombre_usuario\">$nombre_user</span></div>";
+          echo "<div id=\"cuenta\">";
+            echo "<ul>";
+			  echo "<li><a href=\"mi-cuenta.php\" class=\"micuenta\">Mi cuenta</a></li>";			  			 		
+              echo "<li><a href=\"logout.php\" class=\"cerrar\">Cerrar</a></li>";
+            echo "</ul>";
+          echo "</div>";
+        echo "</div>";
+      echo "</div>";
+	  ?>
+	  
 		<a href="contacto.php" class="btnLateral">Contacto</a>
       <div class="caja base-100">
 <!--         <div id="menu">
@@ -107,14 +120,14 @@
       <div class="caja base-100">
         <nav class="navegacion">
           <ul>
-            <li><a href="#" class="seguridad">Seguridad</a></li>
-            <li><a href="#" class="productividad">Productividad</a></li>
-            <li><a href="#" class="responsabilidad">Responsabilidad</a></li>
-            <li><a href="#" class="superacion">Superación</a></li>
-            <li><a href="#" class="optimismo">Optimismo</a></li>
-            <li><a href="#" class="profesionalismo">Profesionalismo</a></li>
-            <li><a href="#" class="catalogo">Catálogo</a></li>
-            <li><a href="#" class="beneficios">Beneficios</a></li>
+            <li><a href="seguridad.php" class="seguridad">Seguridad</a></li>
+            <li><a href="productividad.php" class="productividad">Productividad</a></li>
+            <li><a href="responsabilidad.php" class="responsabilidad">Responsabilidad</a></li>
+            <li><a href="superacion.php" class="superacion">Superación</a></li>
+            <li><a href="optimismo.php" class="optimismo">Optimismo</a></li>
+            <li><a href="profesionalismo.php" class="profesionalismo">Profesionalismo</a></li>
+            <?php echo "<li style=\"$status_cata\"><a href=\"#\" class=\"catalogo\">Catálogo</a></li>"; ?>
+			<?php echo "<li style=\"$status_bene\"><a href=\"#\" class=\"beneficios\">Beneficios</a></li>"; ?>
           </ul>
         </nav>
       </div>
@@ -131,8 +144,8 @@
 	<?php
 	//echo $recurso;
 	
-	if(@$_REQUEST['video1'] != ''){
-		$recurso = @$_REQUEST['video1'];	
+	if(@$_REQUEST['video'] != ''){
+		$recurso = @$_REQUEST['video'];	
 	}else{
 		$recurso = "https://www.youtube.com/embed/dq5TWOYS2G8";
 	}
@@ -147,10 +160,12 @@
 			echo "</div>";
 		  echo "</article>";
 		  echo "<article class=\"mini\">";
-			echo "<div class=\"caja web-25\"><button name=\"video1\" value=\"https://www.youtube.com/embed/dq5TWOYS2G8\" type=\"submit\"><img src=\"img/video0.jpg\"></button></div>";
-			echo "<div class=\"caja web-25\"><button name=\"video1\" value=\"https://www.youtube.com/embed/QIowq_0xjvw\" type=\"submit\"><img src=\"img/video1.jpg\"></button></div>";
-			echo "<div class=\"caja web-25\"><img src=\"img/poster.jpg\"></div>";
-			echo "<div class=\"caja web-25\"><img src=\"img/poster.jpg\"></div>";
+			echo "<div class=\"caja web-25\"><button name=\"video\" value=\"https://www.youtube.com/embed/dq5TWOYS2G8\" type=\"submit\"><img src=\"img/video0.jpg\"></button></div>";
+			echo "<div class=\"caja web-25\"><button name=\"video\" value=\"https://www.youtube.com/embed/QIowq_0xjvw\" type=\"submit\"><img src=\"img/video1.jpg\"></button></div>";
+			echo "<div class=\"caja web-25\"><button name=\"video\" value=\"https://www.youtube.com/embed/z7lPSkCWOcM\" type=\"submit\"><img src=\"img/video2.jpg\"></button></div>";
+			echo "<div class=\"caja web-25\"><button name=\"video\" value=\"https://www.youtube.com/embed/lbzUKOfy0OE\" type=\"submit\"><img src=\"img/video3.jpg\"></button></div>";
+			
+			
 		  echo "</article>";
 		echo "</div>";
 	echo "</form>";
@@ -172,11 +187,14 @@
           </div>
         </div>
       <div class="caja web-25">
-        <form id="footer" class="right">
-          <p>Escríbenos si tienes  dudas o sugerencias</p>
-          <textarea name="textarea" placeholder="Escríbenos si tienes  dudas o sugerencias"> </textarea>
-          <button type="submit">Enviar</button>
-        </form>
+        <form method="post" id="footer" class="right">
+            <p>Escríbenos si tienes  dudas o sugerencias</p>
+            <textarea name="comentario" placeholder="Escríbenos si tienes  dudas o sugerencias"></textarea>
+            <button type="submit">Enviar</button>
+			<?php
+				echo "<input type=\"text\" name=\"rut_send\" value=\"$rut\" hidden=hidden>";	
+			?>
+          </form>
       </div>
     </div>
   </footer>
