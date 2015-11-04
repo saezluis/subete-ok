b403a3693ff354be383fd0ace456fa7c81eb437c
saezluis <lsaez@pm.cl>
2015-11-04 10:04:10 -0300

    Agregados cambios menores

--- beneficios.php
+++ beneficios.php
@@ -113,7 +113,7 @@ exit;
 		if(@$sistema_web=='interno'){
 			$status_bene = 'display:zerocool;';			
 		}else{
-			$status_bene = 'display:none;';
+			$status_bene = 'display:zerocool;';
 		}
 	
 	?>
